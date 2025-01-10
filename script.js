82d3063e4c02faedecf3a334aac42d0418640893
Antony Gilo <51474865+gilosupreme@users.noreply.github.com>
2025-01-10 14:43:55 +0300

    Adding in cookie message div and appear-close functionality

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
-  e.preventDefault();
+  e.preventDefault(); //prevents hrefs with value '#' from taking us to top of the page
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -31,3 +31,20 @@ document.addEventListener('keydown', e => {
     closeModal();
   }
 });
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+const header = document.querySelector('.header');
+header.before(message);
+
+message.style.backgroundColor = '#37383d';
+message.style.overflow = 'hidden';
+message.style.height =
+  Number.parseFloat(window.getComputedStyle(message).height) + 12 + 'px';
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', () => message.remove());
